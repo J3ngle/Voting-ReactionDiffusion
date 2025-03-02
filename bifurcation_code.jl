f639c1eec40e2c4ea798fdce529a6354e062bef4
bmorsky <bryce.morsky@gmail.com>
2025-03-02 17:50:40 -0500

    Update bifurcation_code.jl

--- bifurcation_code.jl
+++ bifurcation_code.jl
@@ -3,7 +3,7 @@ using Plots, LinearAlgebra, Roots
 
 # Parameter setup
 c_vals = range(0, 1, length=10000)  # Range for c
-z = 0.3;  # Set z (adjust as needed)
+z = 0.6;  # Set z (adjust as needed)
 v_equilibrium = Float64[]  # Store equilibrium values
 c_equilibrium = Float64[]  # Store corresponding c values
 stability = Int[]  # 1: stable, 0: unstable, 2: center points
