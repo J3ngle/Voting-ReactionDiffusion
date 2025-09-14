15efe059f2f3e3728399e00fda2aa9c6d7a48add
J3ngle <155997396+J3ngle@users.noreply.github.com>
2025-09-14 10:47:04 -0400

    Update RD_Solver.jl

--- RD_Solver.jl
+++ RD_Solver.jl
@@ -1,25 +1,25 @@
 using DifferentialEquations, Plots, LinearAlgebra, Roots, Statistics, Sundials, ColorSchemes
 @time begin
 # Parameters for computations
-D_c = 1e-3#3 #Diffusion Coefficient for Consensus makers
-D_g = 1e-3#3 #Diffusion Coefficient for Gridlockers
-D_z = 1e-3#3 #Diffusion Coefficient for Zealots
-D_z2 = 1e-3#3 #Diffusion Coefficient for Zealots Party 2
-m_c = 0#1e-3 #Migration rate for Consensus makers
-m_g = 0#1e-3 #Migration rate for Gridlockers
-m_z = 0#1e-3 #Migration rate for Zealots Party 1
-m_z2 =0# 1e-3 #Migration rate for Zealots Party 2
+D_c = 0#1e-10#3 #Diffusion Coefficient for Consensus makers
+D_g = 0#1e-0#3 #Diffusion Coefficient for Gridlockers
+D_z = 0#1e-10#3 #Diffusion Coefficient for Zealots
+D_z2 = 0#1e-0#3 #Diffusion Coefficient for Zealots Party 2
+m_c = 1e-3 #Migration rate for Consensus makers
+m_g = 1e-3 #Migration rate for Gridlockers
+m_z = 1e-3 #Migration rate for Zealots Party 1
+m_z2 =1e-3 #Migration rate for Zealots Party 2
 V=1 #Social Imitation
-λ= 0 #0.5 #Economic preference
+λ= 1 #Economic preference
 b=1#public good benefit
 k=0 #public good cost
-L = 10 #Length of domain 
-Nx, Ny = 10, 10 #Number of discretization points in either direction
+L = 10 #Length of domain    
+Nx, Ny = 15, 15 #Number of discretization points in either direction
 dx = L / (Nx - 1) #Chop up x equally
 dy = L / (Ny - 1) #Chop up y equally
 x = range(0, L, length=Nx) # X size
 y = range(0, L, length=Ny) # y size 
-tfinal=25.0 #Final time
+tfinal=250.0 #Final time
 X, Y = [xi for xi in x, yi in y], [yi for xi in x, yi in y]
 
 #Initial distribution/ conditions
@@ -107,10 +107,10 @@ Fitness_z1(v) = (v).^2 #Strategy fitness for Zealots party 1
 Fitness_z2(v) = (1 .- (v)).^2 #Strategy fitness for Zealots party 2
 
 #Economic Utility functions
-Utility_c(v) = λ .*(b-k) .* v+(1-λ).*Fitness_c(v) #Utility for Consensus makers
-Utility_g(v) = λ .*(b-k) .* v+(1-λ).*Fitness_g(v) #Utility for Gridlockers
-Utility_z1(v) = λ .*(b-k) .* v+(1-λ).*Fitness_z1(v) #Utility for Zealots party 1
-Utility_z2(v) = λ .*(b-k) .* v+(1-λ).*Fitness_z2(v) #Utility for Zealots party 2
+Utility_c(v) = λ .*(b-k) .* v +(1-λ).*Fitness_c(v) #Utility for Consensus makers
+Utility_g(v) = λ .*(b-k) .* v +(1-λ).*Fitness_g(v) #Utility for Gridlockers
+Utility_z1(v) = λ .*(b-k) .* v +(1-λ).*Fitness_z1(v) #Utility for Zealots party 1
+Utility_z2(v) = λ .*(b-k) .* v +(1-λ).*Fitness_z2(v) #Utility for Zealots party 2
 # Initial condition
 u0 = pack(c₀, g₀, z1₀, z2₀, v_c₀, v_g₀)
 du0 = zeros(size(u0))
@@ -200,7 +200,7 @@ c=c ./ population #Normalize c
 g=g ./ population #Normalize g
 z=z ./ population #Normalize z
 z2=z2 ./ population #Normalize z2
-heatmap_population = population ./ total_population #Normalize population
+heatmap_population = population  ./ maximum(population) #Normalize population
 v = (c .* v_c .+ g .* v_g .+ z) #Compute v at the end
 clims = (0, 1) #Color limits for heatmaps
 p1 = heatmap(x, y, c',  aspect_ratio=1,colorbar=false, clims=clims)# clims=clims
@@ -208,22 +208,21 @@ p2 = heatmap(x, y, g',  aspect_ratio=1,colorbar=false, clims=clims)# clims=clims
 p3 = heatmap(x, y, z',  aspect_ratio=1,colorbar=false, clims=clims)# clims=clims
 p4 = heatmap(x, y, z2',  aspect_ratio=1,colorbar=false, clims=clims)# clims=clims
 p5 = heatmap(x, y, heatmap_population',  aspect_ratio=1,colorbar=false, clims=clims) # clims=clims
-p6 = heatmap(x, y, v',  aspect_ratio=1,color=:cool, colorbar=false, clims=clims) # clims=climscolor=:balance,
+p6 = heatmap(x, y, v',  aspect_ratio=1,color=:viridis, colorbar=false, clims=clims) # clims=climscolor=:balance,
 heatmap_figure = plot(p1, p2, p3, p4, p5, p6, layout=(3,3), size=(1400, 1500),colorbar=true, titlefontsize=fontsize, guidefontsize=fontsize, tickfontsize=fontsize, plot_title="Solutions at final time $tfinal")
 display(plot(p1, axis=false, framestyle=:none,ticks=false, size=(625, 625))) #Consensus makers
-#savefig("Heatmap4DifferentM_D=0_C,lambda=$λ,T=$tfinal.pdf")
+#savefig("HeatmapDifferentD_Consensus,lambda=$λ,D=$D_c,T=$tfinal.pdf")
 display(plot(p2, axis=false, framestyle=:none, ticks=false,size=(625, 625))) #Gridlockers
-#savefig("Heatmap4DifferentM_D=0_G,lambda=$λ,T=$tfinal.pdf")
+#savefig("HeatmapDifferentD_Gridlockers,lambda=$λ,D=$D_g,T=$tfinal.pdf")
 display(plot(p3, axis=false, framestyle=:none, ticks=false,size=(625, 625))) #Zealots of party 1
-#savefig("Heatmap4DifferentM_D=0_Z1,lambda=$λ,T=$tfinal.pdf");
+#savefig("HeatmapDifferentD_Z1,lambda=$λ,D=$D_z,T=$tfinal.pdf");
 display(plot(p4, axis=false, framestyle=:none, ticks=false,size=(625, 625))) #Zealots of party 2
-#savefig("Heatmap4DifferentM_D=0_Z2,lambda=$λ,T=$tfinal.pdf")
+#savefig("HeatmapDifferentD_Z2,lambda=$λ,T=$tfinal.pdf")
 display(plot(p5, axis=false, framestyle=:none, ticks=false,size=(625, 625))) #Population
-#savefig("Heatmap4DifferentM_D=0_Population,lambda=$λ,T=$tfinal.pdf")
+#savefig("HeatmapDifferentD_Population,lambda=$λ,T=$tfinal.pdf")
 display(plot(p6, axis=false, framestyle=:none, ticks=false, size=(625,625))) #Vote
-#savefig("Heatmap4DifferentM_D=0_Vote,lambda=$λ,T=$tfinal.pdf")
-display(heatmap_figure)
-#savefig("Heatmap_Clean_DifferentD_EvenIC_Finaltime=$tfinal.pdf")
+#savefig("HeatmapDifferentD_Vote,lambda=$λ,T=$tfinal.pdf")
+display(heatmap_figure)#savefig("Heatmap_Clean_DifferentD_EvenIC_Finaltime=$tfinal.pdf")
 
 # TIME SERIES: Compute averages over the domain at each time step
 time_steps = sol.t
@@ -240,7 +239,7 @@ average_v = [mean(unpack(sol[i])[5]) .* mean(unpack(sol[i])[1])  .+ mean(unpack(
 # Above computes c*v_c + g*v_g + z at each time step
 # Plot averages
 time_series = plot(time_steps, average_c, xlabel="Time", ylabel="Mean",lw=8, xlabelfontsize=20, ylabelfontsize=20,
-     titlefontsize=12, legendfontsize=12, tickfontsize=16, legend=false) #, label="Mean Consensus Makers"
+     titlefontsize=12, legendfontsize=12, tickfontsize=16,ylim=(0,1), xlim=(0,tfinal), legend=false) #, label="Mean Consensus Makers"
 plot!(time_steps, average_g,lw=8)
 plot!(time_steps, average_z,lw=8)
 plot!(time_steps, average_z2,lw=8)
@@ -249,7 +248,7 @@ plot!(time_steps, average_v,lw=8)
 # plot!(time_steps, average_Fitness_z2,lw=8)
 #plot!(time_steps, ts_max_pop, label="Max Population",lw=3)
 display(time_series)
-#savefig("TimeSeriesGridlockT=$tfinal.pdf")
+savefig("TimeSeriesDifferentD=$tfinal.pdf")
 
 ## Time series fitness
 time_series_fit = plot(time_steps, average_Fitness_c, xlabel="Time", ylabel="Mean",lw=8, xlabelfontsize=20, ylabelfontsize=20,
@@ -262,48 +261,48 @@ display(time_series_fit)
 
 
 
-#Sanity check: Plot the average v_c and v_g 
-mean_vc = [mean(unpack(sol[i])[5]) for i in 1:length(time_steps)]
-mean_vg = [mean(unpack(sol[i])[6]) for i in 1:length(time_steps)]
-min_vc = [minimum(unpack(sol[i])[5]) for i in 1:length(time_steps)]
-min_vg = [minimum(unpack(sol[i])[6]) for i in 1:length(time_steps)]
-max_vc = [maximum(unpack(sol[i])[5]) for i in 1:length(time_steps)]
-max_vg = [maximum(unpack(sol[i])[6]) for i in 1:length(time_steps)]
-sanity_population = [sum(unpack(sol[i])[1]) + sum(unpack(sol[i])[2]) + sum(unpack(sol[i])[3]) + sum(unpack(sol[i])[4]) for i in 1:length(time_steps)] ./ [sum(unpack(sol[1])[1]) + sum(unpack(sol[1])[2]) + sum(unpack(sol[1])[3]) + sum(unpack(sol[1])[4])] #./[sum(unpack(sol[1])[1]) + sum(unpack(sol[1])[2]) + sum(unpack(sol[1])[3]) + sum(unpack(sol[1])[4])] # Normalize population
-Population_error = maximum(sanity_population) - minimum(sanity_population)
-SanityCheck=plot(time_steps, mean_vc, label="Mean v_c", xlabel="Time", ylabel="Mean", lw=3, legend=:outertopright, title="Mean v_c and v_g over time, Sanity Check")
-plot!(time_steps, mean_vg, label="Mean v_g", lw=3)
-plot!(time_steps, min_vc, label="Min v_c", lw=3)
-plot!(time_steps, min_vg, label="Min v_g", lw=3)
-plot!(time_steps, max_vc, label="Max v_c", lw=3)
-plot!(time_steps, max_vg, label="Max v_g", lw=3)
-plot!(time_steps, sanity_population, label="Population", lw=3)
-#display(SanityCheck)
-#savefig("MeanTS(SanityCheck)_D_0.01_Finaltime=$tfinal.pdf")
+# #Sanity check: Plot the average v_c and v_g 
+# mean_vc = [mean(unpack(sol[i])[5]) for i in 1:length(time_steps)]
+# mean_vg = [mean(unpack(sol[i])[6]) for i in 1:length(time_steps)]
+# min_vc = [minimum(unpack(sol[i])[5]) for i in 1:length(time_steps)]
+# min_vg = [minimum(unpack(sol[i])[6]) for i in 1:length(time_steps)]
+# max_vc = [maximum(unpack(sol[i])[5]) for i in 1:length(time_steps)]
+# max_vg = [maximum(unpack(sol[i])[6]) for i in 1:length(time_steps)]
+# sanity_population = [sum(unpack(sol[i])[1]) + sum(unpack(sol[i])[2]) + sum(unpack(sol[i])[3]) + sum(unpack(sol[i])[4]) for i in 1:length(time_steps)] ./ [sum(unpack(sol[1])[1]) + sum(unpack(sol[1])[2]) + sum(unpack(sol[1])[3]) + sum(unpack(sol[1])[4])] #./[sum(unpack(sol[1])[1]) + sum(unpack(sol[1])[2]) + sum(unpack(sol[1])[3]) + sum(unpack(sol[1])[4])] # Normalize population
+# Population_error = maximum(sanity_population) - minimum(sanity_population)
+# SanityCheck=plot(time_steps, mean_vc, label="Mean v_c", xlabel="Time", ylabel="Mean", lw=3, legend=:outertopright, title="Mean v_c and v_g over time, Sanity Check")
+# plot!(time_steps, mean_vg, label="Mean v_g", lw=3)
+# plot!(time_steps, min_vc, label="Min v_c", lw=3)
+# plot!(time_steps, min_vg, label="Min v_g", lw=3)
+# plot!(time_steps, max_vc, label="Max v_c", lw=3)
+# plot!(time_steps, max_vg, label="Max v_g", lw=3)
+# plot!(time_steps, sanity_population, label="Population", lw=3)
+# #display(SanityCheck)
+# #savefig("MeanTS(SanityCheck)_D_0.01_Finaltime=$tfinal.pdf")
 
-#println("Population error: ", Population_error)
+# #println("Population error: ", Population_error)
 
 
-nframes = 50
-frame_idxs = round.(Int, range(1, length(sol), length=nframes))
+# nframes = 50
+# frame_idxs = round.(Int, range(1, length(sol), length=nframes))
 
-anim = @animate for idx in frame_idxs
-    c, g, z, z2, v_c, v_g = unpack(sol[idx])
-    population = c .+ g .+ z .+ z2
-    c = c ./ population
-    g = g ./ population
-    z = z ./ population
-    z2 = z2 ./ population
-    heatmap_population = population ./ sum(population)
-    v = (c .* v_c .+ g .* v_g .+ z)
-    clims = (0, 1)
-    p1 = heatmap(x, y, c', aspect_ratio=1, colorbar=false, clims=clims)
-    p2 = heatmap(x, y, g', aspect_ratio=1, colorbar=false, clims=clims)
-    p3 = heatmap(x, y, z', aspect_ratio=1, colorbar=false, clims=clims)
-    p4 = heatmap(x, y, z2', aspect_ratio=1, colorbar=false, clims=clims)
-    p5 = heatmap(x, y, heatmap_population', aspect_ratio=1, colorbar=false, clims=clims)
-    p6 = heatmap(x, y, v', aspect_ratio=1, colorbar=false, color=:cool, clims=clims)
-    plot(p1, p2, p3, p4, p5, p6, layout=(3,3), size=(1400, 1500), colorbar=true, titlefontsize=fontsize, guidefontsize=fontsize, tickfontsize=fontsize, plot_title="Solutions at t=$(round(sol.t[idx], digits=2))")
-end
+# anim = @animate for idx in frame_idxs
+#     c, g, z, z2, v_c, v_g = unpack(sol[idx])
+#     population = c .+ g .+ z .+ z2
+#     c = c ./ population
+#     g = g ./ population
+#     z = z ./ population
+#     z2 = z2 ./ population
+#     heatmap_population = population ./ sum(population)
+#     v = (c .* v_c .+ g .* v_g .+ z)
+#     clims = (0, 1)
+#     p1 = heatmap(x, y, c', aspect_ratio=1, colorbar=false, clims=clims)
+#     p2 = heatmap(x, y, g', aspect_ratio=1, colorbar=false, clims=clims)
+#     p3 = heatmap(x, y, z', aspect_ratio=1, colorbar=false, clims=clims)
+#     p4 = heatmap(x, y, z2', aspect_ratio=1, colorbar=false, clims=clims)
+#     p5 = heatmap(x, y, heatmap_population', aspect_ratio=1, colorbar=false, clims=clims)
+#     p6 = heatmap(x, y, v', aspect_ratio=1, colorbar=false, color=:cool, clims=clims)
+#     plot(p1, p2, p3, p4, p5, p6, layout=(3,3), size=(1400, 1500), colorbar=true, titlefontsize=fontsize, guidefontsize=fontsize, tickfontsize=fontsize, plot_title="Solutions at t=$(round(sol.t[idx], digits=2))")
+# end
 
-mp4(anim, "heatmap_video.mp4", fps=5)
+# mp4(anim, "heatmap_video.mp4", fps=5)
